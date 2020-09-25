89f0b998247ba0727c4286c6f9b93d863554f334
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-09-25 15:17:14 +0300

    generate nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { DateCountPipe } from './date-count.pipe';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    DateCountPipe,
+    QuoteFormComponent,
+    QuoteDetailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
