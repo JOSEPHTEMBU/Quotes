83adeb6d6c5d96e662d5b57f89dbbd2e0a4b9681
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-09-27 01:17:20 +0300

    updated quote.ts

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,9 +1,21 @@
 export class Quote {
-    id: number;
-  name: string;
+  id: number;
+  text: string;
+  author: string;
+  dateAdded: Date;
+  upvote: number;
+  downvote: number;
+  showDetails: boolean;
+  submitter: string;
 
-  constructor(id: number, name:string){
-      this.id = id;
-      this.name = name;
+  constructor(id: number, text: string, author: string, submitter: string, dateAdded: Date) {
+    this.id = id;
+    this.text = text;
+    this.author = author;
+    this.dateAdded = dateAdded;
+    this.upvote = 0;
+    this.downvote = 0;
+    this.showDetails = false;
+    this.submitter = submitter;
   }
 }
