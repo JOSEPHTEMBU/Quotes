2b68e479e9bd6675c6e9f7a9e358ff72156d289c
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-09-27 23:06:57 +0300

    added a cutom directive to the highest votes

--- src/app/strikethrough.directive.ts
+++ src/app/strikethrough.directive.ts
@@ -1,10 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive,ElementRef,Input } from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
 
-  constructor() { }
 
+  constructor(private el: ElementRef) {
+    this.highlight(this.highlightColor || this.defaultColor || 'purple');
+   }
+  @Input() defaultColor: string;
+  @Input('myHighlight') highlightColor: string;
+
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
