fe9eea2d9f751289521ff4a11656e5711fb3dc6a
JOSEPHTEMBU <josephmulama30@gmail.com>
2020-09-27 16:20:39 +0300

    style form

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,9 +7,14 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./quote-form.component.css'],
 })
 export class QuoteFormComponent implements OnInit {
-  
-  
-  
+  newQuote: Quote = new Quote(0, '', '', '', new Date());
+
+  @Output() createQuote = new EventEmitter<Quote>();
+
+  addQuote() {
+    this.createQuote.emit(this.newQuote);
+    this.newQuote = new Quote(0, "", "", "", new Date());
+  }
 
   constructor() {}
 
